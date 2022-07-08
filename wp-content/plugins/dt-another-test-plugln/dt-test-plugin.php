75633c09240545db77be21b6d485d79c429cf0a7
Chris Kraft <ckraft@developertown.com>
2022-07-08 17:04:18 -0400

    updated gitignore to properly handle *.php files

---
+++ wp-content/plugins/dt-another-test-plugln/dt-test-plugin.php
@@ -0,0 +1,26 @@
+<?php
+ /**
+ * @package DT_Another_Test_Plugin
+ * @version 1.0
+ */
+/*
+ 
+Plugin Name: DT Another Test Plugin
+ 
+Plugin URI: https://developertown.com
+ 
+Description: Plugin for general testing.
+ 
+Version: 1.0
+ 
+Author: Chris Kraft
+ 
+Author URI: https://kriggity.com/
+ 
+License: GPLv2 or later
+ 
+*/
+function hello_developertown () {
+    return "<div>Hello Developertown!</div>";
+};
+add_shortcode('hello-dt','hello_developertown');
---
+++ wp-content/plugins/genesis-blocks/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'genesis/blocks',
+        'pretty_version' => '1.5.0',
+        'version' => '1.5.0.0',
+        'reference' => '3a3feef2a74299e8468ebadf44838578b145ac21',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'genesis/blocks' => array(
+            'pretty_version' => '1.5.0',
+            'version' => '1.5.0.0',
+            'reference' => '3a3feef2a74299e8468ebadf44838578b145ac21',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
